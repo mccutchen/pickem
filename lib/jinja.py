ccedc367aa69f1d2fe9a655fb4394a397e3d9378
Will McCutchen <mccutchen@gmail.com>
2010-09-01 17:32:10 -0500

    Add some more Jinja filters

--- lib/jinja.py
+++ lib/jinja.py
@@ -18,7 +18,28 @@ def render_to_string(filename, context={}):
 def dateformat(d, format):
     return getattr(d, 'strftime', lambda x: x)(format)
 
+def key(obj):
+    try:
+        return obj.key()
+    except AttributeError:
+        return obj
+
+def _id(obj):
+    try:
+        return key(obj).id()
+    except AttributeError:
+        return obj
+
+def keyname(obj):
+    try:
+        return key(obj).name()
+    except AttributeError:
+        return obj
+
 environment.filters['dateformat'] = dateformat
+environment.filters['key'] = key
+environment.filters['id'] = _id
+environment.filters['keyname'] = keyname
 
 
 ##############################################################################
