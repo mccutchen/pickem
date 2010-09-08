da357387ab10977233bc09c1e6908a3b09280ebe
Will McCutchen <mccutchen@gmail.com>
2010-09-08 15:35:29 -0500

    Further simplify fixture encoding and decoding by making the hooks directly handle model instances as well.

--- lib/fabric/fixtures.py
+++ lib/fabric/fixtures.py
@@ -21,6 +21,8 @@ def json_encoder(obj):
     elif isinstance(obj, datetime.date):
         return { '__date__': obj.strftime(DATE_FORMAT) }
     elif isinstance(obj, db.Key):
+        # Keys are encoded as a 3-element list of [kind, id_or_name, parent]
+        # where the parent can be null or another key.
         return {
             '__key__': [
                 obj.kind(),
@@ -28,10 +30,18 @@ def json_encoder(obj):
                 json_encoder(obj.parent())
                 ]
             }
+    elif isinstance(obj, db.Model):
+        # Models are encoded as just their key
+        return { '__model__': json_encoder(obj.key()) }
+
+    # There was no special encoding to be done
     return obj
 
 def json_decoder(dct):
-    """Decodes objects encoded as one-item dictionaries. See json_encoder."""
+    """Decodes objects encoded as one-item dictionaries. See
+    json_encoder. NOTE: Models are deserialized as their keys, under the
+    assumption that the place(s?) where a model would be serialized (ie,
+    ReferenceProperty) accept either a model or its key as a value."""
     if len(dct) == 1:
         type_name, value = dct.items()[0]
         type_name = type_name.strip('_')
@@ -42,6 +52,9 @@ def json_decoder(dct):
         elif type_name == 'key':
             kind, keydata, parent = value
             return db.Key.from_path(kind, keydata, parent=parent)
+        elif type_name == 'model':
+            # For models, we just return their key
+            return value
     return dct
 
 
@@ -54,78 +67,50 @@ def load_fixtures(filename):
 
     for count, data in enumerate(json_obj):
         model = get_model(data['model'])
-        if model is None:
-            return
-        create_entity(model, data.get('key'), data['fields'], filename)
+        create_entity(model, data.get('key'), data['fields'])
+
     logging.info("Loaded %d fixtures..." % (count + 1))
 
 def get_model(modelspec):
     """Gets the model class specified in the given modelspec, which should
     be in the format `path.to.models.module.ModelName`."""
+    # Split the modelspec into module path and model name
     module_name, model = modelspec.rsplit('.', 1)
-    try:
-        __import__(module_name, {}, {})
-    except ImportError, e:
-        logging.error('Could not import module for %s' % modelspec)
-        return None
-    else:
-        module = sys.modules[module_name]
-        try:
-            return getattr(module, model)
-        except AttributeError, e:
-            logging.error('Could not find model for %s' % modelspec)
-            return None
+    # Import the module
+    __import__(module_name, {}, {})
+    # Get a reference to the actual module object
+    module = sys.modules[module_name]
+    # Return the model class from the module
+    return getattr(module, model)
 
-def create_entity(model, key, fields, fixture_path):
+def create_entity(model, key, fields):
     """Creates an entity of the given type in the datastore, based on the
     given fields.  ReferenceProperties and ListProperties are containing Keys
     are looked up in the datastore."""
 
     logging.debug('Creating %s entity with key %r' % (model.kind(), key))
 
-    # The final keyword argument we'll pass to the entity's constructor
+    # The final keyword arguments we'll pass to the entity's constructor
     args = { 'key': key }
 
+    # Gather up the field names and values into the args dict.  The names must
+    # be cast to strings to be usable as keyword arguments.
     for field, value in fields.iteritems():
-        # What kind of property does this field need to be?
+        # Any special casing based on property type should happen here
         prop = getattr(model, field)
-
-        # If it's a ReferenceProperty, look it up in the datastore based on
-        # the serialized filter values
-        if isinstance(prop, db.ReferenceProperty):
-            value = db.get(value)
-
-        # If it's a BlobProperty, treat the value as a path, relative to the
-        # current fixture, that contains the data for the property
-        elif isinstance(prop, db.BlobProperty):
-            if value:
-                blob_path = os.path.join(os.path.dirname(fixture_path), value)
-                value = open(blob_path, 'rb').read()
-
-        # Otherwise, assume that the type resulting from JSON deserialization
-        # is appropriate for the datastore
         args[str(field)] = value
 
     # Create and store the entity
-    model(**args).put()
+    return model(**args).put()
 
 def serialize_entities(modelspec):
+    """Serializes all of the entities of the kind specified by the given
+    modelspec as JSON."""
     model = get_model(modelspec)
     fields = model.properties()
-    entities = []
-    for entity in model.all():
-        d = {
-            'model': modelspec,
-            'key': entity.key(),
-            }
-
-        d['fields'] = {}
-        for name, kind in fields.items():
-            value = getattr(entity, name)
-            if isinstance(kind, db.ReferenceProperty):
-                value = value.key()
-            d['fields'][name] = value
-
-        entities.append(d)
-
+    entities = [
+        { 'model': modelspec,
+          'key': entity.key(),
+          'fields': dict((name, getattr(entity, name)) for name in fields) }
+        for entity in model.all()]
     return json.dumps(entities, default=json_encoder, indent=4)
