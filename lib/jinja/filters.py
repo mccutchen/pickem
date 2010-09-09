93691b89251a13728dfc96a899a3e41a11e42c42
Will McCutchen <mccutchen@gmail.com>
2010-09-09 14:53:56 -0500

    Add separate `est` filter instead of magically adjusting inside the date filter

--- lib/jinja/filters.py
+++ lib/jinja/filters.py
@@ -3,13 +3,16 @@ import logging
 import re
 
 
-def date(d, format, fix=True):
+def est(d):
+    """Converts the given datetime or date object from UTC to EST. Used to
+    display game kickoff times."""
+    return d + datetime.timedelta(hours=-5)
+
+def date(d, format):
     """Formats the given datetime object according to the given format string.
     If `fix` is True, the time will automatically be adjusted to from UTC to
     Eastern Standard Time. Leading zeros will be removed from numbers in the
     results."""
-    if fix:
-        d += datetime.timedelta(hours=-5)
     result = d.strftime(format)
     return re.sub(r'0(\d.\d{2})', r'\1', result)
 
