96ba26cd85d00d4b93fdfdbed833e449d1fd2b94
Will McCutchen <mccutchen@gmail.com>
2010-09-10 16:36:03 -0500

    Add a dummy cron job for score updating

--- data/views.py
+++ data/views.py
@@ -8,3 +8,9 @@ class OddsHandler(RequestHandler):
         from data import odds
         logging.info('Updating odds...')
         odds.update_odds()
+
+
+class ScoresHandler(RequestHandler):
+
+    def get(self):
+        logging.info('Updating scores...')
