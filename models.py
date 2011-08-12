dcd79c950afd9e8337e9f70ffb058145831705ea
Will McCutchen <will@keyingredient.com>
2011-08-12 12:20:42 -0400

    Remove unused Week.add_game method

--- models.py
+++ models.py
@@ -106,26 +106,6 @@ class Week(db.Model):
     def find_game_for(self, team):
         return self.games.filter('teams =', team.key()).get()
 
-    def add_game(self, home_team, away_team, **kwargs):
-        commit = kwargs.pop('commit', True)
-        game_key = '%s@%s' % (away_team.slug, home_team.slug)
-        def txn():
-            key = db.Key.from_path('Game', game_key, parent=self.key())
-            game = db.get(key)
-            if not game:
-                game = Game(parent=self,
-                            home_team=home_team,
-                            away_team=away_team,
-                            teams=[home_team.key(), away_team.key()],
-                            **kwargs)
-                if commit:
-                    game.put()
-                created = True
-            else:
-                created = False
-            return game, created
-        return db.run_in_transaction(txn)
-
     @classmethod
     def next(cls):
         if not hasattr(cls, '_next'):
